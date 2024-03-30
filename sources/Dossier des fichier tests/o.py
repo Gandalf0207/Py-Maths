3e121062314b696719dafe134fa3301cea684948
Gandalf0207 <quentin.pladeau@gmail.com>
2024-03-30 21:37:42 +0100

    avancement de la refonte de l'exo en test
    Former-commit-id: 61dc2d9353b2600b30bfdbe53b12131b74b2ed8e

--- sources/Dossier des fichier tests/o.py
+++ sources/Dossier des fichier tests/o.py
@@ -30,22 +30,22 @@ with doc.create(Section("Section de test", numbering = False)):
     doc.append(NoEscape("\\begin{align*}"))
 
     nb_value = 1
-    nb1 = random.randint(1,10)
-    nb2 = random.randint(1,10)
-    nb3 = random.randint(1,10)
-    nb4 = random.randint(1,10)
-    nb5 = random.randint(1,10)
-    nb6 = random.randint(1,10)
+    nb1 = random.randint(2,20)
+    nb2 = random.randint(2,20)
+    nb3 = random.randint(2,20)
+    nb4 = random.randint(2,20)
+    nb5 = random.randint(2,20)
+    nb6 = random.randint(2,20)
         
 
-        
+
     ### LIGNE DE L'EXO ###
 
     doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x + %s &= \\frac{%s}{%s} - %sx \\\\" % (nb1,nb2,nb3,nb4,nb5,nb6)))
     doc.append(Command('vspace', '5mm'))
     doc.append(NewLine())
 
-    ### LIGNE 1 ###
+
 
     #On regarde s'ils sont différents pour savoir si on doit simplifier directement la fraction ou non 
     if (nb2 != nb1) and (nb4!=nb5):
@@ -64,13 +64,96 @@ with doc.create(Section("Section de test", numbering = False)):
         doc.append(Command('vspace', '5mm'))
         doc.append(NewLine())
 
-    else :
-        if (nb1==nb2):
+        doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x + %sx &= \\frac{%s}{%s} -%sx + %sx - %s\\\\" % (nb1,nb2,nb6,nb4,nb5,nb6,nb6,nb3)))
+        doc.append(Command('vspace', '5mm'))
+        doc.append(NewLine())
 
-            #Ils sont pareils donc on affiche 1 (frac1)
+        doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x + %sx &= \\frac{%s}{%s} - %s\\\\" % (nb1,nb2,nb6,nb4,nb5,nb3)))
+        doc.append(Command('vspace', '5mm'))
+        doc.append(NewLine())
+
+        #On met tout les valeurs sur le même dénominateurs pour pouvoir les additionner
+        nb6 = nb2*nb6
+        nb3 = nb5*nb3
+
+        doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x + \\frac{%s}{%s}x &= \\frac{%s}{%s} - \\frac{%s}{%s}\\\\" % (nb1,nb2,nb6,nb2,nb4,nb5,nb3,nb5)))
+        doc.append(Command('vspace', '5mm'))
+        doc.append(NewLine())
+
+        #On simplifie en additionnant les fraction entre elles
+        nbx = nb1 + nb6
+        nb = nb4 - nb3
+
+        doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x &= \\frac{%s}{%s} \\\\" % (nbx,nb2,nb,nb5)))
+        doc.append(Command('vspace', '5mm'))
+        doc.append(NewLine())
+
+        #On multiplie par l'inverse de la fraction de x pour obtenir la valeur pour x
+
+        doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x \\times \\frac{%s}{%s} &= \\frac{%s}{%s} \\times \\frac{%s}{%s} \\\\" % (nbx,nb2,nb2,nbx,nb,nb5,nb2,nbx)))
+        doc.append(Command('vspace', '5mm'))
+        doc.append(NewLine())
+
+        #On calcule cette multiplication de fraction
+
+            #calcul resultat fraction
+        nb_num = nb*nb2
+        nb_den = nb5*nbx
+
+            #calcul du pgcd si c'est possible d'arrondir
+        pgcd_frac_result = pgcd(nb_num,nb_den)
+        nb_num = nb_num//pgcd_frac_result
+        nb_den = nb_den//pgcd_frac_result
+
+        doc.append(NoEscape("\\  \\Leftrightarrow x &= \\frac{%s}{%s}  \\\\" % (nb_num,nb_den)))
+        doc.append(Command('vspace', '5mm'))
+        doc.append(NewLine())
+
+    else :
+        if (nb1==nb2) and (nb4==nb5):
+
+            #Ils sont pareils donc on affiche 1 (frac1&2)
+            doc.append(NoEscape("\\  \\Leftrightarrow x +%s - %s +%sx &= %s -%sx +%sx - %s \\\\" % (nb3,nb3,nb6,nb_value,nb6,nb6,nb3)))
+            doc.append(Command('vspace', '5mm'))
+            doc.append(NewLine())
+
+            #On developpe
+            doc.append(NoEscape("\\  \\Leftrightarrow x + %sx &= %s -%s \\\\" % (nb6,nb_value,nb3)))
+            doc.append(Command('vspace', '5mm'))
+            doc.append(NewLine())
+
+            #On developpe
+            nbx = 1 + nb6
+            nb = 1 - nb3
+
+            doc.append(NoEscape("\\  \\Leftrightarrow %sx &= -%s \\\\" % (nbx,nb)))
+            doc.append(Command('vspace', '5mm'))
+            doc.append(NewLine())
+
+            #On divise par x pour obtenir la valeur de x
+
+            doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%sx}{%s} &= \\frac{%s}{%s} \\\\" % (nbx,nbx,nb,nbx)))
+            doc.append(Command('vspace', '5mm'))
+            doc.append(NewLine())
+
+            #On simplifie et on calcule le pgcd si il est nécésaire
+
+            pgcd_frac1 = pgcd(nb,nbx)
+            nb = nb//pgcd_frac1
+            nbx = nbx//pgcd_frac1
+
+            doc.append(NoEscape("\\  \\Leftrightarrow x &= \\frac{%s}{%s} \\\\" % (nb,nbx)))
+            doc.append(Command('vspace', '5mm'))
+            doc.append(NewLine())
+
+
+
+        elif (nb1==nb2):
+            #Ils sont pareils donc on affiche 1 (frac1&2)
             doc.append(NoEscape("\\  \\Leftrightarrow x +%s - %s &= \\frac{%s}{%s} -%sx - %s \\\\" % (nb3,nb3,nb4,nb5,nb6,nb3)))
             doc.append(Command('vspace', '5mm'))
             doc.append(NewLine())
+
         else:
             #Ils sont pareils donc on affiche 1 (frac2)
             doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x + %s - %s &= %s -%sx - %s\\\\" % (nb1,nb2,nb3,nb3,nb_value,nb6,nb3)))
@@ -78,7 +161,6 @@ with doc.create(Section("Section de test", numbering = False)):
             doc.append(NewLine())
 
 
-    ### LIGNE 2 ###
 
 
         doc.append(NoEscape("\\\\"))
