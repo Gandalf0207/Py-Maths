ec5854afc8f988e4c545af24c6bc4e1cf5d34c79
Gandalf0207 <quentin.pladeau@gmail.com>
2024-04-02 21:28:29 +0200

    s
    Former-commit-id: 427a6d9a8132787754268a2a2cc5672be71d8c0d

--- sources/Exo_Equation_premier_degre/Fichiers_Equations/Nv1_1.py
+++ sources/Exo_Equation_premier_degre/Fichiers_Equations/Nv1_1.py
@@ -30,96 +30,43 @@ def correction_exo(doc,nb1,nb2,nb3,nb4,nb5,nb6,nb_value):
     #On regarde s'ils sont différents pour savoir si on doit simplifier directement la fraction ou non 
     if (nb2 != nb1) and (nb4!=nb5):
 
-        # Fonction de pgcd pour pouvoir simplifier la fraction au maximum
-        pgcd_frac1 = pgcd(nb1,nb2)
-        nb1 = nb1//pgcd_frac1
-        nb2 = nb2//pgcd_frac1
+        doc.append(NoEscape("\\ \\text{L'équation à résoudre est : } " % ()))
 
-        pgcd_frac2 = pgcd(nb4,nb5)
-        nb4 = nb4//pgcd_frac2
-        nb5 = nb5//pgcd_frac2
+        doc.append(NoEscape("\\begin{align*}"))
+        doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x + %s &= \\frac{%s}{%s} - %sx \\\\" % (nb1,nb2,nb3,nb4,nb5,nb6)))
+        doc.append(NoEscape("\\end{align*}"))
 
-        # Les fractions sont simplifiées si c'est possible
-        doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x + %s - %s &= \\frac{%s}{%s} -%sx - %s\\\\" % (nb1,nb2,nb3,nb3,nb4,nb5,nb6,nb3)))
+
+        # Etape 1
+        deno_com = nb2*nb5
+        doc.append(NoEscape("\\  \\parbox{ 450pt }{ \\textbf{Etape 1} : Simplifions les fractions. Pour simplifier, multiplions chaque terme de l’équation par le dénominateur commun, qui est (%s) (le produit de (%s) et (%s)) : }" % (deno_com,nb2,nb5)))
         
-        doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x + %sx &= \\frac{%s}{%s} -%sx + %sx - %s\\\\" % (nb1,nb2,nb6,nb4,nb5,nb6,nb6,nb3)))
-
-        doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x + %sx &= \\frac{%s}{%s} - %s\\\\" % (nb1,nb2,nb6,nb4,nb5,nb3)))
-
-        #On met tout les valeurs sur le même dénominateurs pour pouvoir les additionner
-        nb6 = nb2*nb6
-        nb3 = nb5*nb3
-        doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x + \\frac{%s}{%s}x &= \\frac{%s}{%s} - \\frac{%s}{%s}\\\\" % (nb1,nb2,nb6,nb2,nb4,nb5,nb3,nb5)))
-
-        #On simplifie en additionnant les fraction entre elles
-        nbx = nb1 + nb6
-        nb = nb4 - nb3
-        doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x &= \\frac{%s}{%s} \\\\" % (nbx,nb2,nb,nb5)))
-
-        #On multiplie par l'inverse de la fraction de x pour obtenir la valeur pour x
-        doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x \\times \\frac{%s}{%s} &= \\frac{%s}{%s} \\times \\frac{%s}{%s} \\\\" % (nbx,nb2,nb2,nbx,nb,nb5,nb2,nbx)))
-
-        #On calcule cette multiplication de fraction
-
-        #calcul resultat fraction
-        nb_num = nb*nb2
-        nb_den = nb5*nbx
-
-        #calcul du pgcd si c'est possible d'arrondir
-        pgcd_frac_result = pgcd(nb_num,nb_den)
-        nb_num = nb_num//pgcd_frac_result
-        nb_den = nb_den//pgcd_frac_result
-        doc.append(NoEscape("\\  \\Leftrightarrow x &= \\frac{%s}{%s}  \\\\" % (nb_num,nb_den)))
+        doc.append(NoEscape("\\begin{align*}"))
+        doc.append(NoEscape("\\  \\Leftrightarrow %s \\cdot \\left(\\frac{%s}{%s}x\\right) + %s \\cdot %s &= %s \\cdot \\left(\\frac{%s}{%s}\\right) - %s \\cdot %sx \\\\" % (deno_com,nb1,nb2,deno_com,nb3, deno_com,nb4,nb5,deno_com,nb6)))
+        nb = nb2*nb3
+        nbx = nb2 * nb6
+        doc.append(NoEscape("\\  \\Leftrightarrow %sx + %s &= %s - %sx \\\\" % (nb1,nb,nb2,nbx)))
+        doc.append(NoEscape("\\end{align*}"))
 
 
     else :
         if (nb1==nb2) and (nb4==nb5):
 
-            #Ils sont pareils donc on affiche 1 (frac1&2)
-            doc.append(NoEscape("\\  \\Leftrightarrow x +%s - %s +%sx &= %s -%sx +%sx - %s \\\\" % (nb3,nb3,nb6,nb_value,nb6,nb6,nb3)))
+            doc.append(NoEscape("\\ \\text{L'équation à résoudre est : } " % ()))
 
-            #On developpe
-            doc.append(NoEscape("\\  \\Leftrightarrow x + %sx &= %s -%s \\\\" % (nb6,nb_value,nb3)))
-
-            #On developpe
-            nbx = nb_value + nb6
-            nb = nb_value - nb3
-            doc.append(NoEscape("\\  \\Leftrightarrow %sx &= -%s \\\\" % (nbx,nb)))
-
-            #On divise par x pour obtenir la valeur de x
-            doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%sx}{%s} &= \\frac{%s}{%s} \\\\" % (nbx,nbx,nb,nbx)))
-
-            #On simplifie et on calcule le pgcd si il est nécésaire
-            pgcd_frac1 = pgcd(nb,nbx)
-            nb = nb//pgcd_frac1
-            nbx = nbx//pgcd_frac1
-            doc.append(NoEscape("\\  \\Leftrightarrow x &= \\frac{%s}{%s} \\\\" % (nb,nbx)))
+            doc.append(NoEscape("\\begin{align*}"))
+            doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x + %s &= \\frac{%s}{%s} - %sx \\\\" % (nb1,nb2,nb3,nb4,nb5,nb6)))
+            doc.append(NoEscape("\\end{align*}"))
 
 
         elif (nb1==nb2):
-            #Ils sont pareils donc on affiche 1 (frac1&2)
-            doc.append(NoEscape("\\  \\Leftrightarrow x +%s - %s &= \\frac{%s}{%s} -%sx - %s \\\\" % (nb3,nb3,nb4,nb5,nb6,nb3)))
 
-            #on calcule le pgcd pour simplifier la fraction
-            
-            pgcd_frac2 = pgcd(nb4,nb5)
-            nb4 = nb4//pgcd_frac2
-            nb5 = nb5//pgcd_frac2
-            # on déplace les éléments que chaque coté
-            doc.append(NoEscape("\\  \\Leftrightarrow x +%sx &= \\frac{%s}{%s} -%sx +%sx - %s \\\\" % (nb6,nb4,nb5,nb6,nb6,nb3)))
+            doc.append(NoEscape("\\ \\text{L'équation à résoudre est : } " % ()))
 
-            # on passe les elements sur fraction du coté ou cela est nécéssaire et on simplifie les x
-            nb3 = nb3*nb5
-            nbx = nb6 + nb_value
-
-            doc.append(NoEscape("\\  \\Leftrightarrow %sx &= \\frac{%s}{%s} - \\frac{%s}{%s} \\\\" % (nbx,nb4,nb5,nb3,nb5)))
-
-            #on passe tout sur la meme fraction à droite 
-            nb = nb4 - nb3
-            doc.append(NoEscape("\\  \\Leftrightarrow %sx &= \\frac{%s}{%s}  \\\\" % (nbx,nb,nb5)))
-
-
+            doc.append(NoEscape("\\begin{align*}"))
+            doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s}x + %s &= \\frac{%s}{%s} - %sx \\\\" % (nb1,nb2,nb3,nb4,nb5,nb6)))
             doc.append(NoEscape("\\end{align*}"))
+
 
         else:
 
