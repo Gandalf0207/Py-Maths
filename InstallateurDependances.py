e4129fee7f01a2b12c5e4a145f0cde3c969e0eff
Gandalf <quentin.pladeau@gmail.com>
2024-10-17 22:12:10 +0200

    [+] Base refonte système à 2 inconnues

---
+++ InstallateurDependances.py
@@ -0,0 +1,66 @@
+import subprocess
+import sys
+
+# Liste des packages nécessaires
+packages = [
+    "lastpage",
+    "geometry",
+    "tkz-tab",
+    "fancyhdr",
+    "newunicodechar",
+    "latexmk"
+]
+
+def check_miktex_package(package):
+    """Vérifie si un package MiKTeX est installé."""
+    try:
+        # Exécute la commande mpm pour vérifier l'installation du package
+        result = subprocess.run(
+            ["mpm", "--list", package],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+        
+        # Si le package est trouvé, la sortie standard le contiendra
+        if package in result.stdout:
+            return True
+        return False
+    
+    except FileNotFoundError:
+        print("MiKTeX Package Manager non trouvé. Assurez-vous que MiKTeX est installé et que 'mpm' est dans votre PATH.")
+        sys.exit(1)
+
+def install_miktex_package(package):
+    """Installe un package MiKTeX."""
+    try:
+        print(f"Installation de {package}...")
+        subprocess.run(
+            ["mpm", "--install", package],
+            check=True
+        )
+        print(f"{package} a été installé avec succès.")
+    except subprocess.CalledProcessError as e:
+        print(f"Échec de l'installation de {package}. Erreur: {e}")
+
+def main():
+    for package in packages:
+        if check_miktex_package(package):
+            print(f"{package} est déjà installé.")
+        else:
+            print(f"{package} n'est pas installé.")
+            install_miktex_package(package)
+
+    # Vérifie si pip est installé et l'installe si nécessaire
+    subprocess.run(["python", "-m", "ensurepip"], check=True)
+
+    # Installe les dépendances à partir du fichier Requirements.txt
+    try:
+        subprocess.run(["pip", "install", "-r", "Requirements.txt"], check=True)
+        print("Les dépendances ont été installées avec succès.")
+    except subprocess.CalledProcessError as e:
+        print(f"Erreur lors de l'installation des dépendances: {e}")
+
+
+
+
+if __name__ == "__main__":
+    main()
--- sources/Exo_Equation_deux_inconnues/Equation_2_inconnues.py
+++ sources/Exo_Equation_deux_inconnues/Equation_2_inconnues.py
@@ -2,19 +2,17 @@
 
 ## Importation des Librairies : 
 
-# Module de création du Fichier Tex et convertion en pdf et autre
-from pylatex import *
-from pylatex.utils import *
-
-#Modules de calcul...
-import math
-import random
-
-
-
+from settings import *
 
 
 def write(doc,i):
+
+    def pgcd(a,b):
+        # pgcd(a,b): calcul du 'Plus Grand Commun Diviseur' entre les 2 nombres entiers a et b
+        while b != 0:
+            a,b=b,a%b
+        return a
+
     # on génère les valeurs aléatoirement pour avoir de nouveau exercices !
     nb1 = random.randint(1,50)
     nb2 = random.randint(1,50)
@@ -22,23 +20,6 @@ def write(doc,i):
     nb4 = random.randint(1,50)
     nb5 = random.randint(1,50)
     nb6 = random.randint(1,50)
-    nb7 = random.randint(1,50)
-    nb8 = random.randint(1,50)
-    nb9 = random.randint(1,50)
-    nb10 = random.randint(1,50)
-    nb11= random.randint(1,50)
-
-    # valeurs légèrement différentes pour le niveau 3 pour éviter de trop gros nombres
-    nb1_nv3 = random.randint(1,10)
-    nb2_nv3 = random.randint(1,10)
-    nb3_nv3 = random.randint(1,10)
-    nb4_nv3 = random.randint(1,10)
-    nb5_nv3 = random.randint(1,10)
-    nb6_nv3 = random.randint(1,10)
-    nb7_nv3 = random.randint(1,10)
-    nb8_nv3 = random.randint(1,10)
-    nb9_nv3 = random.randint(1,10)
-    nb10_nv3 = random.randint(1,10)
 
 # on définit la taille de l'écriture pour le document
     doc.append(pylatex.Command('fontsize', arguments = ['12', '10']))
@@ -67,29 +48,6 @@ def write(doc,i):
         doc.append(NoEscape("\\end{align*}"))
 
 
-# Niveau 2 exo
-        doc.append("Niveau 2 :")
-
-        doc.append(NoEscape("\\begin{align*}"))
-        doc.append(NoEscape("\\begin{cases}"))
-        doc.append(NoEscape("\\  \\frac{%s}{%s}x + %sy &= %sy - %s \\\\" % (nb1, nb2, nb3, nb4, nb5)))
-        doc.append(NoEscape("\\  %sx + %sy &= %sy + \\frac{%s}{%s} \\\\" % (nb6,nb7 ,nb8,nb9,nb10 )))
-        doc.append(NoEscape("\\end{cases}"))
-        doc.append(NoEscape("\\\\"))
-        doc.append(NoEscape("\\end{align*}"))
-
-# Niveau 3 exo
-        doc.append("Niveau 3 :")
-
-        doc.append(NoEscape("\\begin{align*}"))
-        doc.append(NoEscape("\\begin{cases}"))
-        doc.append(NoEscape("\\  %s(%sy + %sx) &= %s \\\\" % (nb1_nv3, nb2_nv3,nb3_nv3, nb4_nv3)))
-        doc.append(NoEscape("\\  %sx + %sy &= %s(%sy + %s) \\\\" % (nb5_nv3,nb6_nv3, nb7_nv3, nb8_nv3, nb9_nv3)))
-        doc.append(NoEscape("\\end{cases}"))
-        doc.append(NoEscape("\\\\"))
-        doc.append(NoEscape("\\end{align*}"))
-
-
 # On ajoute une page entre la correction et les exercices
     doc.append(NewPage())
 
@@ -106,245 +64,119 @@ def write(doc,i):
     doc.append(NoEscape("\\end{align*}"))
 
 #puis on développe chaque étape avec un commentaire de ce que l'on fait
-    doc.append(NoEscape("\\ \\text{L'équation ligne 2 permet d'écrire :}\\\\" % ()))
-
+    #Etape 1 
+    doc.append(NoEscape("\\  \\parbox{ 450pt }{ \\textbf{Etape 1} : Multiplier chaque équation pour aligner les coefficients d'une variable. Pour aligner les coefficients de x, nous pouvons multiplier les deux équations pour obtenir le même coefficient de x. Par exemple, nous pouvons multiplier l'équation 1 par %s et l'équation 2 par %s: }" % (nb3,nb1)))
 
     doc.append(NoEscape("\\begin{align*}"))
-    doc.append(NoEscape("\\  \\Leftrightarrow %sx + %sy - %sy &= %s - %sy \\\\" % (nb3,nb4,nb4,nb6,nb4)))
+    doc.append(NoEscape("\\  %s \\times (%sx + %sy) &= %s \\times %s \\\\" % (nb3, nb1, nb2, nb5, nb3)))
+    doc.append(NoEscape("\\  %s \\times (%sx + %sy) &= %s \\times %s \\\\" % (nb1, nb3, nb4, nb6, nb1)))
+    doc.append(NoEscape("\\end{align*}"))
 
-    doc.append(NoEscape("\\  \\Leftrightarrow %sx &= -%sy + %s\\\\" % (nb3, nb4,nb6)))
+    newNb1 = nb3*nb1
+    newNb2 = nb3*nb2
+    newNb5 = nb3*nb5
+    newNb3 = nb1*nb3
+    newNb4 = nb1*nb4
+    newNb6 = nb1*nb6
+    doc.append(NoEscape("\\begin{align*}"))
+    doc.append(NoEscape("\\  %sx + %sy &= %s \\\\" % (newNb1, newNb2, newNb5)))
+    doc.append(NoEscape("\\  %sx + %sy &= %s \\\\" % (newNb3, newNb4, newNb6)))
+    doc.append(NoEscape("\\end{align*}"))
 
-    doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%sx}{%s} &= \\frac{-%sy + %s}{%s}\\\\" % (nb3,nb3,nb4,nb6,nb3)))
+    #Etape 2 
+    doc.append(NoEscape("\\  \\parbox{ 450pt }{ \\textbf{Etape 2} : Soustraire les équations. En soustrayant l'équation 1 de l'équation 2, nous éliminons la variable x:}"))
+
+    doc.append(NoEscape("\\begin{align*}"))
+    doc.append(NoEscape("\\ \\Leftrightarrow  (%sx + %sy) - (%sx + %sy) &= %s - %s\\\\" % (newNb1, newNb2, newNb3, newNb4, newNb5, newNb6)))
+    newY = newNb2 - newNb4
+    newValue = newNb5 - newNb6
+    doc.append(NoEscape("\\ \\Leftrightarrow  %sy &= %s\\\\" % (newY, newValue)))
+    doc.append(NoEscape("\\end{align*}"))
+
+    #Etape 3 
+    doc.append(NoEscape("\\  \\parbox{ 450pt }{ \\textbf{Etape 3} : Résoudre pour y}"))
+    division = pgcd(newValue, newY)
+    newValue = newValue// division
+    newY = newY // division
+
+    doc.append(NoEscape("\\begin{align*}"))
+    if newY ==1:
+        doc.append(NoEscape("\\ \\Leftrightarrow  y  &= %s\\\\" % (newValue)))
+    else:
+        doc.append(NoEscape("\\ \\Leftrightarrow  y  &= \\frac{%s}{%s} \\\\" % (newValue, newY)))
     doc.append(NoEscape("\\end{align*}"))
     
-    doc.append(NoEscape("\\\\"))
-
-
-    doc.append(NoEscape("\\ \\text{On remplace x par $\\frac{-%sy + %s}{%s}$ dans l'équation 1}\\\\" % (nb4,nb6,nb3)))
-
-
-
-    doc.append(NoEscape("\\begin{align*}"))
-    doc.append(NoEscape("\\ \\Leftrightarrow  %s \\times \\frac{-%sy + %s}{%s} + %sy &= %s\\\\" % (nb1,nb4,nb6,nb3,nb2,nb5)))
-
-    doc.append(NoEscape("\\ \\Leftrightarrow  %s \\times \\frac{-%sy}{%s} + %s \\times \\frac{%s}{%s} + %sy &= %s\\\\" % (nb1,nb4,nb3,nb1,nb6, nb3,nb2,nb5)))
-
-    new_y_1_nv1 = round((nb1*((-nb4)/nb3) + nb2),2)
-    new_nb_1_nv1 = round((nb1*(nb6 / nb3)),2)
-    doc.append(NoEscape("\\ \\Leftrightarrow  %sy + %s &= %s\\\\" % (new_y_1_nv1, new_nb_1_nv1,nb5)))
-
-    doc.append(NoEscape("\\ \\Leftrightarrow  %sy + %s - %s &= %s - %s\\\\" % (new_y_1_nv1, new_nb_1_nv1,new_nb_1_nv1,nb5,new_nb_1_nv1)))
-
-    new_nb_2_nb1 = round(nb5 - new_nb_1_nv1,2)
-    doc.append(NoEscape("\\ \\Leftrightarrow  %sy  &= %s\\\\" % (new_y_1_nv1, new_nb_2_nb1)))
-
-    doc.append(NoEscape("\\ \\Leftrightarrow  \\frac{%sy}{%s}  &= \\frac{%s}{%s}\\\\" % (new_y_1_nv1,new_y_1_nv1, new_nb_2_nb1,new_y_1_nv1)))
-
-    result_y_nv1 = round((new_nb_2_nb1 / new_y_1_nv1),2)
-    doc.append(NoEscape("\\ \\Leftrightarrow  y  &\\approx %s\\\\" % (result_y_nv1)))
-    doc.append(NoEscape("\\end{align*}"))
-
-
-
-    doc.append(NoEscape("\\ \\text{En remplaçant y par la valeur obtenue dans l'équation 1 :}\\\\" % ()))
-
-
-
-    doc.append(NoEscape("\\begin{align*}"))
-    doc.append(NoEscape("\\ \\Leftrightarrow  %sx + %s \\times %s &= %s \\\\" % (nb1,nb2,result_y_nv1,nb5)))
-
-    nb = round((result_y_nv1*nb2),2)
-    doc.append(NoEscape("\\ \\Leftrightarrow  %sx + %s -%s &= %s - %s \\\\" % (nb1,nb,nb,nb5, nb)))
-
-    new_nb_3_nv1 = round(nb5 - nb,2)
-    doc.append(NoEscape("\\ \\Leftrightarrow  %sx  &= %s \\\\" % (nb1, new_nb_3_nv1)))
-
-    doc.append(NoEscape("\\ \\Leftrightarrow  \\frac{%sx}{%s} &= \\frac{%s}{%s}\\\\" % (nb1, nb1,new_nb_3_nv1,nb1)))
-
-    approche = round((new_nb_3_nv1/nb1),2)
-    doc.append(NoEscape("\\ \\Leftrightarrow  x &\\approx %s \\\\" % (approche)))
-    doc.append(NoEscape("\\end{align*}"))
-
-
-
-
-
-
-
-    doc.append(NoEscape("\\\\"))
-    doc.append(NewPage())
-
-
-
-
-
-# de meme pour la correction du niveau 2
-    doc.append("Niveau 2 :")
-
-    doc.append(NoEscape("\\begin{align*}"))
-    doc.append(NoEscape("\\begin{cases}"))
-    doc.append(NoEscape("\\  \\frac{%s}{%s}x + %sy &= %sy - %s \\\\" % (nb1, nb2, nb3, nb4, nb5)))
-    doc.append(NoEscape("\\  %sx + %sy &= %sy + \\frac{%s}{%s} \\\\" % (nb6,nb7 ,nb8,nb9,nb10 )))
-    doc.append(NoEscape("\\end{cases}"))
-    doc.append(NoEscape("\\\\"))
-    doc.append(NoEscape("\\end{align*}"))
-
-
-    doc.append(NoEscape("\\ \\text{L'équation ligne 1 permet d'écrire :}\\\\" % ()))
-
-
-    doc.append(NoEscape("\\begin{align*}"))
-    doc.append(NoEscape("\\  \\Leftrightarrow  \\frac{%s}{%s}x + %sy - %sy &= %sy - %s - %sy \\\\" % (nb1, nb2, nb3,nb3, nb4, nb5,nb3)))
-
-    new_y_1_nv2 = nb4-nb3
-    doc.append(NoEscape("\\  \\Leftrightarrow  \\frac{%s}{%s}x &= %sy - %s \\\\" % (nb1, nb2, new_y_1_nv2, nb5)))
-
-    doc.append(NoEscape("\\  \\Leftrightarrow  \\frac{%s}{%s}x \\times \\frac{%s}{%s} &= (%sy - %s) \\times \\frac{%s}{%s} \\\\" % (nb1, nb2,nb2,nb1, new_y_1_nv2, nb5,nb2,nb1)))    
-
-    new_y_2_nv2 = round(new_y_1_nv2/(nb1/nb2),2)
-    new_nb_1_nv2 = round(nb5/(nb1/nb2),2)
-    doc.append(NoEscape("\\  \\Leftrightarrow  x &= %sy - %s\\\\" % (new_y_2_nv2,new_nb_1_nv2)))    
-    doc.append(NoEscape("\\end{align*}"))
     
-
-
-    doc.append(NoEscape("\\\\"))
-
-
-    doc.append(NoEscape("\\ \\text{On remplace x par $%sy - %s$ dans l'équation 2}\\\\" % (new_y_2_nv2,new_nb_1_nv2)))
-
-
+    #Etape 4
+    doc.append(NoEscape("\\  \\parbox{ 450pt }{ \\textbf{Etape 4} : Substituer y dans l'une des équations d'origine pour résoudre pour x}"))
 
     doc.append(NoEscape("\\begin{align*}"))
-    doc.append(NoEscape("\\ \\Leftrightarrow  %s(%sy - %s) + %sy &= %sy + \\frac{%s}{%s} \\\\" % (nb6,new_y_2_nv2,new_nb_1_nv2,nb7,nb8,nb9,nb10)))
+    if newY ==1 : 
+        doc.append(NoEscape("\\ \\Leftrightarrow  %sx + %s &= %s\\\\" % (nb1, nb2, nb5)))
+        doc.append(NoEscape("\\ \\Leftrightarrow  %sx &= %s - %s\\\\" % (nb1, nb5, nb2)))
+        newValue2 = nb5 - nb2
+        doc.append(NoEscape("\\ \\Leftrightarrow  %sx  &= %s \\\\" % (nb1, newValue2)))
+        division = pgcd(nb1, newValue2)
+        newX = nb1 // division
+        newValue2 = newValue2 // division
 
-    new_y_3_nv2 = round((nb6*new_y_2_nv2),3)
-    new_nb_2_nv2 = round((nb6*new_nb_1_nv2),3)
-    doc.append(NoEscape("\\ \\Leftrightarrow  %sy - %s + %sy &= %sy + \\frac{%s}{%s} \\\\" % (new_y_3_nv2,new_nb_2_nv2,nb7,nb8,nb9,nb10)))
+        if newX ==1:
+            doc.append(NoEscape("\\ \\Leftrightarrow  x  &= %s \\\\" % (newValue2)))
+        else:
+            doc.append(NoEscape("\\ \\Leftrightarrow  x  &= \\frac{%s}{%s} \\\\" % (newValue2, newX)))
+    else:
+        doc.append(NoEscape("\\ \\Leftrightarrow  %sx + %s \\times \\frac{%s}{%s} &= %s\\\\" % (nb1, nb2, newValue, newY, nb5)))
+        newValue_ = nb2*newValue
+        division = pgcd(newValue_, newY)
+        newValue_ = newValue_// division
+        newY_ = newY // division
+        if newY_ ==1:
+            doc.append(NoEscape("\\ \\Leftrightarrow  %sx + %s &= %s\\\\" % (nb1, newValue_, nb5)))
+            doc.append(NoEscape("\\ \\Leftrightarrow  %sx &= %s - %s\\\\" % (nb1, nb5, newValue_)))
+            newValue2 = nb5 - newValue_
+            doc.append(NoEscape("\\ \\Leftrightarrow  %sx  &= %s \\\\" % (nb1, newValue2)))
+            division = pgcd(nb1, newValue2)
+            newX = nb1 // division
+            newValue2 = newValue2 // division
 
-    doc.append(NoEscape("\\ \\Leftrightarrow  %sy - %s +%s + %sy &= %sy + \\frac{%s}{%s} +%s \\\\" % (new_y_3_nv2,new_nb_2_nv2,new_nb_2_nv2,nb7,nb8,nb9,nb10,new_nb_2_nv2)))
+            if newX ==1:
+                doc.append(NoEscape("\\ \\Leftrightarrow  x  &= %s \\\\" % (newValue2)))
+            else:
+                doc.append(NoEscape("\\ \\Leftrightarrow  x  &= \\frac{%s}{%s} \\\\" % (newValue2, newX)))
+        else:
+            doc.append(NoEscape("\\ \\Leftrightarrow  %sx + \\frac{%s}{%s}  &= %s\\\\" % (nb1, newValue_, newY_, nb5)))
+            doc.append(NoEscape("\\ \\Leftrightarrow  %sx  &= %s - \\frac{%s}{%s}\\\\" % (nb1, nb5, newValue_, newY_)))
+            newValue2 = newValue_ - nb5*newY_  
+            division = pgcd(newValue2, newY_)
+            newValue2 = newValue2// division
+            newY_ = newY_ // division
 
-    new_nb_4_nv2 = round(((nb9 / nb10) + new_nb_2_nv2),2)
-    doc.append(NoEscape("\\ \\Leftrightarrow  %sy +%sy - %sy &= %sy -%sy + %s \\\\" % (new_y_3_nv2,nb7,nb8,nb8,nb8,new_nb_4_nv2)))
+            if newY_ ==1:
+                doc.append(NoEscape("\\ \\Leftrightarrow  %sx  &= %s \\\\" % (nb1, newValue2)))
+                division = pgcd(nb1, newValue2)
+                newX = nb1 // division
+                newValue2 = newValue2 // division
 
-    new_y_4_nv2 = round(new_y_3_nv2 +nb7 - nb8,2)
-    doc.append(NoEscape("\\ \\Leftrightarrow  %sy &= %s \\\\" % (new_y_4_nv2, new_nb_4_nv2)))
+                if newX ==1:
+                    doc.append(NoEscape("\\ \\Leftrightarrow  x  &= %s \\\\" % (newValue2)))
+                else:
+                    doc.append(NoEscape("\\ \\Leftrightarrow  x  &= \\frac{%s}{%s} \\\\" % (newValue2, newX)))
+            else:
+                doc.append(NoEscape("\\ \\Leftrightarrow  %sx  &= \\frac{%s}{%s} \\\\" % (nb1, newValue2, newY_)))
+                doc.append(NoEscape("\\ \\Leftrightarrow  x  &= \\frac{%s}{%s} \\times \\frac{1}{%s} \\\\" % (newValue2, newY_, nb1)))
+                newX = newY_*nb1
+                division = pgcd(newValue2, newX)
+                newValue2 = newValue2// division
+                newX = newX // division
 
-    doc.append(NoEscape("\\ \\Leftrightarrow  \\frac{%sy}{%s} &= \\frac{%s}{%s} \\\\" % (new_y_4_nv2,new_y_4_nv2, new_nb_4_nv2,new_y_4_nv2)))
-
-    result_y_nv2 = round((new_nb_4_nv2/ new_y_4_nv2),2)
-    doc.append(NoEscape("\\ \\Leftrightarrow y &\\approx %s \\\\" % (result_y_nv2)))
-    doc.append(NoEscape("\\end{align*}"))
-
-
-    doc.append(NoEscape("\\ \\text{En remplaçant y par la valeur obtenue dans l'équation 2 :}\\\\" % ()))
-
-
-    doc.append(NoEscape("\\begin{align*}"))
-    doc.append(NoEscape("\\  %sx + %s \\times %s &= %s \\times %s + \\frac{%s}{%s} \\\\" % (nb6,nb7, result_y_nv2 ,nb8,result_y_nv2,nb9,nb10 )))
-
-    new_nb_5_nv2 = round((nb7*result_y_nv2),2)
-    new_nb_6_nv2 = round((nb8 * result_y_nv2),2)
-    doc.append(NoEscape("\\ \\Leftrightarrow %sx + %s - %s &= %s +\\frac{%s}{%s} -%s \\\\" % (nb6, new_nb_5_nv2, new_nb_5_nv2, new_nb_6_nv2,nb9,nb10, new_nb_5_nv2 )))
-
-    new_nb_7_nv2 = round((new_nb_6_nv2 + (nb9/nb10)-new_nb_5_nv2),2)
-    doc.append(NoEscape("\\ \\Leftrightarrow %sx &= %s \\\\" % (nb6, new_nb_7_nv2)))
-
-    doc.append(NoEscape("\\ \\Leftrightarrow \\frac{%sx}{%s} &= \\frac{%s}{%s} \\\\" % (nb6,nb6 ,new_nb_7_nv2,nb6)))
-
-    result_x_nv_2 = round((new_nb_7_nv2/nb6),2)
-    doc.append(NoEscape("\\ \\Leftrightarrow x &\\approx %s \\\\" % (result_x_nv_2)))
-    doc.append(NoEscape("\\end{align*}"))
-
-
-
-    doc.append(NoEscape("\\\\"))
-    doc.append(NewPage())
-
-
-
-# de meme pour la correction du niveau 3
-    doc.append("Niveau 3 :")
-
-    doc.append(NoEscape("\\begin{align*}"))
-    doc.append(NoEscape("\\begin{cases}"))
-    doc.append(NoEscape("\\  %s(%sy + %sx) &= %s \\\\" % (nb1_nv3, nb2_nv3,nb3_nv3, nb4_nv3)))
-    doc.append(NoEscape("\\  %sx + %sy &= %s(%sy + %s) \\\\" % (nb5_nv3,nb6_nv3, nb7_nv3, nb8_nv3, nb9_nv3)))
-    doc.append(NoEscape("\\end{cases}"))
-    doc.append(NoEscape("\\\\"))
-    doc.append(NoEscape("\\end{align*}"))
-
-
-    doc.append(NoEscape("\\ \\text{L'équation ligne 1 permet d'écrire :}\\\\" % ()))
-
-
-    doc.append(NoEscape("\\begin{align*}"))
-    doc.append(NoEscape("\\  \\Leftrightarrow %s \\times %sy + %s \\times %sx &= %s \\\\" % (nb1_nv3, nb2_nv3,nb1_nv3,nb3_nv3, nb4_nv3)))
-
-    new_y_1_nv1 = (nb1_nv3*nb2_nv3)
-    new_x_1_nv3 = (nb1_nv3*nb3_nv3)
-    doc.append(NoEscape("\\  \\Leftrightarrow %sy + %sx &= %s \\\\" % (new_y_1_nv1, new_x_1_nv3, nb4_nv3)))
-
-    doc.append(NoEscape("\\  \\Leftrightarrow %sy - %sy + %sx &= %s - %sy \\\\" % (new_y_1_nv1, new_y_1_nv1, new_x_1_nv3, nb4_nv3, new_y_1_nv1)))
-
-    doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%sx}{%s} &= \\frac{%s - %sy}{%s} \\\\" % (new_x_1_nv3,new_x_1_nv3, nb4_nv3, new_y_1_nv1,new_x_1_nv3)))
-
-    doc.append(NoEscape("\\  \\Leftrightarrow x &= \\frac{%s - %sy}{%s} \\\\" % (nb4_nv3, new_y_1_nv1,new_x_1_nv3)))
-    doc.append(NoEscape("\\end{align*}"))
-
+                if newX ==1:
+                    doc.append(NoEscape("\\ \\Leftrightarrow  x  &= %s \\\\" % (newValue2)))
+                else:
+                    doc.append(NoEscape("\\ \\Leftrightarrow  x  &= \\frac{%s}{%s} \\\\" % (newValue2, newX))) 
     
-    doc.append(NoEscape("\\\\"))
+    doc.append(NoEscape("\\end{align*}"))          
 
 
-    doc.append(NoEscape("\\ \\text{On remplace x par $\\frac{%s - %sy}{%s}$ dans l'équation 2}\\\\" % (nb4_nv3, new_y_1_nv1,new_x_1_nv3)))
-
-    doc.append(NoEscape("\\begin{align*}"))
-    doc.append(NoEscape("\\ \\Leftrightarrow %s \\times \\frac{%s - %sy}{%s} + %sy &= %s \\times %sy + %s \\times %s \\\\" % (nb5_nv3,nb4_nv3, new_y_1_nv1,new_x_1_nv3,nb6_nv3, nb7_nv3,nb8_nv3 ,nb7_nv3, nb9_nv3)))
-    
-    new_y_2_nv3 = (nb7_nv3*nb8_nv3)
-    new_nb_2_nv3 = (nb7_nv3*nb9_nv3)
-    doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s \\times (%s - %sy)}{%s} + %sy - %sy &= %sy - %sy + %s \\\\" % (nb5_nv3,nb4_nv3, new_y_1_nv1,new_x_1_nv3,nb6_nv3,new_y_2_nv3,new_y_2_nv3,new_y_2_nv3,new_nb_2_nv3)))
-
-    new_nb_3_nv3 = (nb5_nv3*nb4_nv3)
-    new_y_3_nv3 = (nb5_nv3*new_y_1_nv1)
-    doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s - %sy}{%s} + %sy - %sy &= %s \\\\" % (new_nb_3_nv3,new_y_3_nv3,new_x_1_nv3,nb6_nv3,new_y_2_nv3,new_nb_2_nv3)))
-
-    doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s} - \\frac{%sy}{%s} + %sy -%sy &= %s \\\\" % (new_nb_3_nv3,new_x_1_nv3,new_y_3_nv3,new_x_1_nv3,nb6_nv3,new_y_2_nv3,new_nb_2_nv3)))
-
-    new_y_4_nv3 = round(((- new_y_3_nv3 / new_x_1_nv3) + nb6_nv3 - new_y_2_nv3),2)
-    doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%s}{%s} - \\frac{%s}{%s} + %sy &= %s - \\frac{%s}{%s} \\\\" % (new_nb_3_nv3,new_x_1_nv3,new_nb_3_nv3,new_x_1_nv3,new_y_4_nv3, new_nb_2_nv3,new_nb_3_nv3,new_x_1_nv3)))
-
-    new_nb_5_nv3 = round((new_nb_2_nv3 +(- new_nb_3_nv3 / new_x_1_nv3)),2)
-    doc.append(NoEscape("\\  \\Leftrightarrow %sy &= %s \\\\" % (new_y_4_nv3, new_nb_5_nv3)))
-
-    doc.append(NoEscape("\\  \\Leftrightarrow \\frac{%sy}{%s} &= \\frac{%s}{%s} \\\\" % (new_y_4_nv3,new_y_4_nv3,new_nb_5_nv3,new_y_4_nv3)))
-
-    result_y_nv3 = round((new_nb_5_nv3 / new_y_4_nv3),2)
-    doc.append(NoEscape("\\  \\Leftrightarrow y &\\approx %s \\\\" % (result_y_nv3)))
-    doc.append(NoEscape("\\end{align*}"))
+### Partis triviales sur les simplifications car jamais simplifiables, repasser plus tard
 
 
-    doc.append(NoEscape("\\ \\text{En remplaçant y par la valeur obtenue dans l'équation 2 :}\\\\" % ()))
-
-
-    doc.append(NoEscape("\\begin{align*}"))
-    doc.append(NoEscape("\\  %s(%s \\times %s + %sx) &= %s \\\\" % (nb1_nv3, nb2_nv3,result_y_nv3 ,nb3_nv3, nb4_nv3)))
-
-    new_nb_6_nv3 = round((nb2_nv3 * result_y_nv3),2)
-    doc.append(NoEscape("\\  %s \\times %s + %s \\times %sx &= %s \\\\" % (nb1_nv3, new_nb_6_nv3,nb1_nv3 ,nb3_nv3, nb4_nv3)))
-
-    new_nb_7_nv7 = round((nb1_nv3 * new_nb_6_nv3),2)
-    new_x_2_nv3 = round((nb1_nv3*nb3_nv3),2)
-    doc.append(NoEscape("\\  %s - (%s) +  %sx &= %s -(%s)\\\\" % (new_nb_7_nv7,new_nb_7_nv7,new_x_2_nv3, nb4_nv3,new_nb_7_nv7)))
-
-    new_nb_8_nv3 = round((nb4_nv3 - new_nb_7_nv7),2)
-    doc.append(NoEscape("\\  %sx &= %s\\\\" % (new_x_2_nv3,new_nb_8_nv3)))
-
-    doc.append(NoEscape("\\  \\frac{%sx}{%s} &= \\frac{%s}{%s}\\\\" % (new_x_2_nv3,new_x_2_nv3,new_nb_8_nv3,new_x_2_nv3)))
-
-    result_x_nv3 = round((new_nb_8_nv3 / new_x_2_nv3),2)
-    doc.append(NoEscape("\\  x &\\approx %s\\\\" % (result_x_nv3)))
-    doc.append(NoEscape("\\end{align*}"))
-
-
-### FIN DE L'EXO ÉQUATION à 2 INCONNUES ###
